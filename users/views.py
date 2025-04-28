faab2a510223426591a5948d0faa0df6fad9cf68
anisa <anisahime@gmail.com>
2025-04-29 03:12:30 +1000

    finally fixes

--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ class CustomUserCreateAPIView(CreateAPIView):
     permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
-        """Метод для переопределения регистрации по email"""
+        """Метод для проверки авторизованных пользователей"""
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
