f823f99e4147dfbb67337cea814b9f93b5143827
anisa <anisahime@gmail.com>
2025-06-21 01:04:46 +1000

    fixes errors

--- config/settings.py
+++ config/settings.py
@@ -10,12 +10,75 @@ load_dotenv(override=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dummy_test_key_12345")
+
 # Проверка окружения для тестов
 TESTING = os.getenv("GITHUB_ACTIONS") == "true" or "test" in sys.argv
 
-if TESTING:
-    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dummy_test_key_12345")
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
+ALLOWED_HOSTS = ["*"]
+
+# Application definition
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "django_filters",
+    "rest_framework_simplejwt",
+    "drf_yasg",
+    "django_celery_beat",
+    "users",
+    "materials",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+# Database
+if TESTING:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -27,60 +90,6 @@ if TESTING:
         }
     }
 else:
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True if os.getenv("DEBUG") == "True" else False
-
-    ALLOWED_HOSTS = ["*"]
-
-    # Application definition
-    INSTALLED_APPS = [
-        "django.contrib.admin",
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sessions",
-        "django.contrib.messages",
-        "django.contrib.staticfiles",
-        "rest_framework",
-        "django_filters",
-        "rest_framework_simplejwt",
-        "drf_yasg",
-        "django_celery_beat",
-        "users",
-        "materials",
-    ]
-
-    MIDDLEWARE = [
-        "django.middleware.security.SecurityMiddleware",
-        "django.contrib.sessions.middleware.SessionMiddleware",
-        "django.middleware.common.CommonMiddleware",
-        "django.middleware.csrf.CsrfViewMiddleware",
-        "django.contrib.auth.middleware.AuthenticationMiddleware",
-        "django.contrib.messages.middleware.MessageMiddleware",
-        "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    ]
-
-    ROOT_URLCONF = "config.urls"
-
-    TEMPLATES = [
-        {
-            "BACKEND": "django.template.backends.django.DjangoTemplates",
-            "DIRS": [],
-            "APP_DIRS": True,
-            "OPTIONS": {
-                "context_processors": [
-                    "django.template.context_processors.request",
-                    "django.contrib.auth.context_processors.auth",
-                    "django.contrib.messages.context_processors.messages",
-                ],
-            },
-        },
-    ]
-
-    WSGI_APPLICATION = "config.wsgi.application"
-
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -92,82 +101,66 @@ else:
         }
     }
 
-    REST_FRAMEWORK = {
-        "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
-        "DEFAULT_AUTHENTICATION_CLASSES": [
-            "rest_framework_simplejwt.authentication.JWTAuthentication",
-        ],
-        "DEFAULT_PERMISSION_CLASSES": [
-            "rest_framework.permissions.IsAuthenticated",
-        ],
-    }
 
-    # Password validation
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-        },
-        {
-            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-        },
-        {
-            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-        },
-        {
-            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-        },
-    ]
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
-    # Internationalization
-    LANGUAGE_CODE = "en-us"
+# Internationalization
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Europe/Moscow"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
-    TIME_ZONE = "Europe/Moscow"
+# Настройки статики
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-    USE_I18N = True
-    USE_L10N = True
-    USE_TZ = True
+# Настройки медиа
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-    # Настройки статики
-    STATIC_URL = "static/"
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# Настройки для аутентификации
+AUTH_USER_MODEL = "users.CustomUser"
 
-    # Настройки медиа
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# Настройки времени жизни JWT-токенов
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
-    DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# Настройки Celery
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
+CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
 
-    # Настройки для аутентификации
-    AUTH_USER_MODEL = "users.CustomUser"
-
-    # Настройки времени жизни JWT-токенов
-    SIMPLE_JWT = {
-        "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-        "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    }
-
-    STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
-
-    # Настройки Celery
-    # URL-адрес брокера сообщений
-    CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
-
-    # URL-адрес брокера результатов, также Redis
-    CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
-    CELERY_TIMEZONE = "Europe/Moscow"
-    CELERY_TASK_TRACK_STARTED = True
-    CELERY_TASK_TIME_LIMIT = 30 * 60
-
-    # Настройки почтового сервиса
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.yandex.ru"
-    EMAIL_PORT = 465
-    EMAIL_USE_TLS = False
-    EMAIL_USE_SSL = True
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    SERVER_EMAIL = EMAIL_HOST_USER
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# Настройки почтового сервиса
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+SERVER_EMAIL = os.getenv("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
